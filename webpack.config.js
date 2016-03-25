688d68e69bf97c3d8a76294322ff604c5df5da3a
byronwasti <byron.wasti@gmail.com>
2016-03-25 13:41:35 -0400

    Setting up build for server

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss       = require('precss');
+
+module.exports = {
+    entry: ['webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+        './react_components/App.jsx'],
+    output: {
+        //path: path.join(__dirname, '/public/'),
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+        {
+            //tell webpack to use jsx-loader for all *.jsx files
+            test: [/\.js$/,/\.jsx$/],
+            exclude: '/node_modules/',
+            loader: 'babel-loader',
+            query: {
+                cacheDirectory: true,
+                presets: ['react', 'es2015']
+            }
+        },
+        {
+            test:   [/\.css$/, /\.scss$/],
+            //loader: ['style-loader','css-loader','postcss-loader', 'sass']
+            loaders: ['style','css','postcss-loader', 'sass']
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.scss', '.css']
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
---
+++ webpack.config.production.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+var precss       = require('precss');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './react_components/App.jsx',
+    output: {
+        path: path.join(__dirname, '/public/'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [ 
+        {
+            //tell webpack to use jsx-loader for all *.jsx files
+            test: [/\.js$/,/\.jsx$/],
+            exclude: '/node_modules/',
+            loader: 'babel-loader',
+            query: {
+                cacheDirectory: true,
+                presets: ['react', 'es2015']
+            }
+        },
+        {
+            test:   [/\.css$/, /\.scss$/],
+            loaders: ['style','css','postcss-loader', 'sass']
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.scss', '.css']
+    },
+    postcss: function () {
+        return [autoprefixer, precss];
+    },
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        })
+    ]
+};
