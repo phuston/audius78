b6fb4ba116ccb40ab11d66dd059bbae3940ae344
Patrick <patrickhuston1@gmail.com>
2016-04-03 19:56:27 -0400

    Adds scaffolding for basic reducers

---
+++ client/reducers/row.js
@@ -0,0 +1,19 @@
+import { handleActions } from 'redux-actions';
+
+export default handleActions({
+	ADD_ROW: (state, action) => (
+		// TODO: do some shit here
+	),
+	REMOVE_ROW: (state, action) => (
+		// TODO: do some other shit here
+	),
+	FLAG_TRACK: (state, action) => (
+		// TODO: do some shit here
+	),
+	SPLIT_TRACK: (state, action) => (
+		// TODO: do some shit here
+	),
+	MOVE: TRACK: (state, action) => (
+		// TODO: do some shit here
+	)
+}, {})
---
+++ client/reducers/user.js
@@ -0,0 +1,10 @@
+import { handleActions } from 'redux-actions';
+
+export default handleActions({
+	ADD_USER: (state, action) => (
+		// TODO: do some shit here
+	),
+	REMOVE_USER: (state, action) => (
+		// TODO: do some other shit here
+	)
+}, {})
