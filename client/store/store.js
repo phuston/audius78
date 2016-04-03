ce7922e1dc6e464f2388cb1f3cb6796c2dbde303
Patrick <patrickhuston1@gmail.com>
2016-04-03 14:40:23 -0400

    Creates store

---
+++ client/store/store.js
@@ -0,0 +1,25 @@
+import { compose, applyMiddleware, createStore } from 'redux';
+import rootReducer from '../reducers/root.js';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
+
+import { browserHistory } from 'react-router';
+import { syncHistory } from 'redux-simple-router';
+
+let middlewares = [promise];
+
+const reduxRouter = syncHistory(browserHistory);
+middlewares.push(reduxRouter);
+
+if (process.env.NODE_ENV != 'production') {
+    const createLogger = require(`redux-logger`);
+    const logger = createLogger({collapse:true});
+    middlewares.push(logger);
+}
+
+const devTools = window.devToolsExtension ? window.devToolsExtension() : f => f;
+let store = createStore(rootReducer, compose(applyMiddleware(...middlewares), devTools));
+
+reduxRouter.listenForReplays(store);
+
+export default store;
