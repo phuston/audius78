e9376850079ebc523830100414534b9768177d88
Patrick <patrickhuston1@gmail.com>
2016-04-14 18:07:36 -0400

    Creates workspace model

---
+++ models/workspace.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose") ;
+
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
+
+var workspacesSchema = mongoose.Schema({
+  _id: {type: String},
+  rows: [{
+    rowId: String,
+    rawAudio: String,
+    audioBlocks: [{
+      length: Number,
+      row_offset: Number,
+      file_offset: Number,
+      flags: [{
+        start_time: Number,
+        duration: Number,
+        type: String
+      }]
+    }]
+  }]
+});
+
+
+module.exports = mongoose.model('Workspace', howlsSchema);
